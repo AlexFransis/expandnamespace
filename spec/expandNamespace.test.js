464418aeb8147be1ed1f0cc321aee3ab2f924292
AlexFransis <Ghost@ghost-2.local>
2018-04-26 18:57:40 -0400

    first commit

---
+++ spec/expandNamespace.test.js
@@ -0,0 +1,43 @@
+const proxyquire = require( 'proxyquire' );
+
+describe( 'The ./lib/expandNamespaces function', ()=>{
+
+    it( 'should keep paths without namespaces unchanged', ()=>{
+
+        const expandNamespaces = proxyquire( '../lib/expandNamespace', {
+            './loadNamespaces': sinon.stub()
+        } );
+
+        const expected = './somePath';
+        const actual   = expandNamespaces( expected );
+
+        expect( actual ).to.equal( actual );
+
+    } );
+
+    it.skip( 'start writing tests here', ()=>{
+
+    } );
+
+    beforeEach(() => {
+        const expandNamespaces = proxyquire('../lib/expandNamespace.js', {
+            './loadNamespaces': () => {}
+        });
+    });
+
+    it('should return any invalid modules passed as argument', () => {
+        const invalidModule = "30004";
+        const expected = invalidModule;
+
+        expect(expandNamespaces(invalidModule)).to.equal(expected);
+
+    });
+
+    it('should raise an assertion error if the module is not loaded', () => {
+        const modulePath = "<namespace.js>";
+        const namespaceKey = "namespace.js";
+        const errMsg = `namespace <${namespaceKey}> is not defined.`;
+
+        expandNamespaces(modulePath).should.Throw(AssertionError({message: errMsg}));
+    });
+} );

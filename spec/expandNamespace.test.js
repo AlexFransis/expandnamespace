27b24807b9aa6da03f94cec7d8341e5c524682e0
AlexFransis <Ghost@ghost-2.local>
2018-04-29 17:39:56 -0400

    added missing files and fix some tests

--- spec/expandNamespace.test.js
+++ spec/expandNamespace.test.js
@@ -1,50 +1,62 @@
 const proxyquire = require( 'proxyquire' );
+const assert     = require( 'assert' );
 
 describe( 'The ./lib/expandNamespaces function', ()=>{
 
-    it( 'should keep paths without namespaces unchanged', ()=>{
+  it( 'should keep paths without namespaces unchanged', ()=>{
 
-        const expandNamespaces = proxyquire( '../lib/expandNamespace', {
-            './loadNamespaces': sinon.stub()
-        } );
-
-        const expected = './somePath';
-        const actual   = expandNamespaces( expected );
-
-        expect( actual ).to.equal( actual );
-
+    const expandNamespaces = proxyquire( '../lib/expandNamespace', {
+      './loadNamespaces': sinon.stub()
     } );
 
-    it.skip( 'start writing tests here', ()=>{
+    const expected = './somePath';
+    const actual   = expandNamespaces( expected );
 
+    expect( actual ).to.equal( expected );
+
+  } );
+
+  it.skip( 'start writing tests here', ()=>{
+
+  } );
+
+  let expandNS;
+
+  beforeEach( ()=>{
+
+    expandNS = proxyquire( '../lib/expandNamespace', {
+      './loadNamespaces': ()=>( { namespaces: { module2: 'path/to' } } )
     } );
 
-    beforeEach(() => {
-        const expandNamespaces = proxyquire('../lib/expandNamespace.js', {
-            './loadNamespaces': () => {}
-        });
-    });
+  } );
 
-    it('should return any invalid modules passed as argument', () => {
-        const invalidModule = "30004";
-        const expected = invalidModule;
+  it( 'should return any invalid modules passed as argument', ()=>{
 
-        expect(expandNamespaces(invalidModule)).to.equal(expected);
+    const invalidModule = '30004';
+    const expected      = invalidModule;
 
-    });
+    expect( expandNS( invalidModule ) ).to.equal( expected );
 
-    it('should raise an assertion error if the module is not loaded', () => {
-        const modulePath = "<namespace.js>";
-        const namespaceKey = "namespace.js";
-        const errMsg = `namespace <${namespaceKey}> is not defined.`;
+  } );
 
-        expandNamespaces(modulePath).should.Throw(AssertionError({message: errMsg}));
-    });
+  it( 'should raise an assertion error if the module is not loaded', ()=>{
 
-    it('should return a relative path that starts with .', () => {
-        const modulePath = "module.js";
-        const expected = "./../path/to/module";
+    const modulePath   = '<module1>';
+    const namespaceKey = 'module1';
+    const { errMsg }   = new assert.AssertionError(
+      { message: `namespace <${ namespaceKey }> is not defined.` } );
 
-        expect(expandNamespaces(modulePath)).to.equal(expected);
-    });
+    expect( ()=>expandNS( modulePath ) ).to.throw( errMsg );
+
+  } );
+
+  it( 'should return a relative path that starts with .', ()=>{
+
+    const actual   = expandNS( '<module2>', '/' ).charAt( 0 );
+    const expected = '.';
+
+    expect( actual ).to.equal( expected );
+
+  } );
+
 } );
